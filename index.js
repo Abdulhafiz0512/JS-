df8309dddc9231bc7e1bc4a743c98999402f8734
Abdulhafiz <psfer10green10@gmail.com>
2024-03-28 18:18:56 +0500

    done

---
+++ index.js
@@ -0,0 +1,115 @@
+// Integer2
+// var mass=+prompt("massani kiriting: ");
+// window.alert(mass/1000 + ' tonna');
+
+
+// Integer3
+// var hajm=+prompt("fayl hajmini kiriting: ");
+// window.alert((hajm-hajm%1024)/1024 + ' KB');
+
+// Integer4
+// var a=+prompt("A: ");
+// var b=+prompt("B: ");
+// window.alert(a/b + " ta kesma mavjud");
+
+// Integer5
+// var c=prompt("A: ")*1;
+// var d=prompt("B: ")*1;
+// window.alert(c/d%1 + " qismi ortib qoladi");
+
+// Integer6
+// var son=prompt("ikki xonali sonni kiriting: ")*1;
+// window.alert("o'nliklar xonasi: "+(son-son%10)/10+" birliklar xonasi: "+son%10);
+
+// Integer7
+// var son=prompt("ikki xonali sonni kiriting: ")*1;
+// window.alert("Raqamlari yig'indisi: "+((son-son%10)/10+son%10));
+
+// Integer8
+// var son=prompt("ikki xonali sonni kiriting: ")*1;
+// window.alert("Natija: "+son%10+(son-son%10)/10);
+
+// Integer9
+// var son=prompt("Uch xonali sonni kiriting: ")*1;
+// window.alert("Yuzlar xonasi: "+(son-son%100)/100);
+
+// Integer10
+// var son=prompt("Uch xonali sonni kiriting: ")*1;
+// window.alert("Birliklar xonasi: "+son%10+" O'nliklar xonasi: "+(son-son%10)/10+" Yuzlar xonasi: "+(son-son%100)/100);
+
+// Integer11
+// var son=prompt("Uch xonali sonni kiriting: ")*1;
+// window.alert("Raqamlari yig'indisi: "+((son-son%10)/10+son%10+(son-son%100)/100));
+
+
+// Integer12
+// var son=prompt("Uch xonali sonni kiriting: ")*1;
+// var uch=son%10;
+// var ikki=(son%100-son%10)/10;
+// var bir=(son-son%100)/100;
+// window.alert("Natija: "+uch+ikki+bir)
+
+// HOMEWORK
+
+// Integer14
+// var son=prompt("Sonni kiriting: ")*1;
+// var uch=son%10; 
+// var ikki=(son%100-son%10)/10; 
+// var bir=(son-son%100)/100; 
+// window.alert("Natija: "+uch+bir+ikki);
+
+// Integer15
+// var son=prompt("Sonni kiriting: ")*1;
+// var uch=son%10; 
+// var ikki=(son%100-son%10)/10; 
+// var bir=(son-son%100)/100;
+// window.alert("Natija: "+ikki+bir+uch);
+
+// Integer16
+// var son=prompt("Sonni kiriting: ")*1;
+// var uch=son%10; 
+// var ikki=(son%100-son%10)/10; 
+// var bir=(son-son%100)/100;
+// window.alert("Natija: "+bir+uch+ikki);
+
+// Integer17
+// var son=prompt("Sonni kiriting: ")*1;
+// var yuzlar=(son%1000-son%100)/100;
+// window.alert("Natija: "+yuzlar);
+
+// Integer18
+// var son=prompt("Sonni kiriting: ")*1;
+// var minglar=(son-son%1000)/1000;
+// window.alert("Natija: "+minglar);
+
+// Integer19
+// var N=prompt("Sekundlarni kiriting: ")*1;
+// var qoldiq=N%60;
+// var natija=(N-qoldiq)/60;
+// window.alert(natija+" minut utgan");
+
+// Integer20
+// var N=prompt("Sekundlarni kiriting: ")*1;
+// var qoldiq=N%3600;
+// var natija=(N-qoldiq)/3600;
+// window.alert(natija+" soat utgan");
+
+// Integer21
+// var N=prompt("Sekundlarni kiriting: ")*1;
+// var qoldiq=N%60;
+// var natija=(N-qoldiq)/60;
+// window.alert(natija+" minut "+qoldiq + " sekund utgan");
+
+// Integer22
+// var N=prompt("Sekundlarni kiriting: ")*1;
+// var qoldiq=N%3600;
+// var natija=(N-qoldiq)/3600;
+// window.alert(natija+" soat "+qoldiq + " sekund utgan");
+
+// Integer23
+// var N=prompt("Sekundlarni kiriting: ")*1;
+// var soat=(N-N%3600)/3600;
+// var minut=(N%3600-N%60)/60;
+// var sekund=N%60;
+// window.alert(soat+ " soat "+minut+" minut "+sekund + " sekund utgan");
+
