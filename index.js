40e3b643c3a07c9c0d3598ffac7f695d3243cb56
Abdulhafiz <psfer10green10@gmail.com>
2024-06-06 02:19:41 +0500

    new homework

--- index.js
+++ index.js
@@ -1,88 +1,49 @@
-(function () {
-  let time = document.getElementById("time");
-  time.style.fontSize = "32px";
-  let sana = document.getElementById("sana");
-  time.style.fontSize ="28px";
-  let vaqt = document.getElementById("vaqt");
-  vaqt.style.fontSize ="32px";
-   
-  setInterval(function () {
-    let my_birthday = new Date('2025,05,12')
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let kun = date.getDate();
-    let year = date.getFullYear();
-    let hafta =date.getDay();
-    let mon =date.getMonth();
-    let months=[
-      'yanvar',
-      'fevral',
-      'mart',
-      'aprel',
-      'may',
-      'iyun',
-      'iyul',
-      'avgust',
-      'sentyabr',
-       'oktyabr',
-       'noyabr',
-       'dekabr' 
-
-    ]
-    let kunlari =[
-      'dushanba',
-      'seshanba',
-      'chorshanba',
-      'payshanba',
-      'juma',
-      'shanba',
-      'yakshanba'
-    ]
+// .then() catch
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    let userListHTML = '<ul>';
+    users.forEach(user => {
+      userListHTML += `
+        <li>
+          <strong>Name:</strong> ${user.name}<br>
+          <strong>Email:</strong> ${user.email}<br>
+          <strong>Phone:</strong> ${user.phone}<br>
+          <strong>Street:</strong> ${user.address.street}<br>
+        </li>
+      `;
+    });
+    userListHTML += '</ul>';
     
-    let left =my_birthday-date
-    console.log(left)
-    let left_miliseconds = left%1000
-    left = (left-left_miliseconds)/ 1000
-    let left_seconds = left%60
-    left = (left-left_seconds)/60
-    let left_minutes = left %60
-    left = (left-left_minutes)/60
-    let left_hours = left%24
-    left = (left-left_hours)/24
-    let left_days = left%365
-    
-
-    if (left_seconds<10){
-      left_seconds='0'+left_seconds;
-    }
-    if (left_minutes<10){
-      left_minutes='0'+left_minutes;
-    }
-    if (left_hours<10){
-      left_hours='0'+left_hours;
-    }
-    
-    if (seconds<10){
-      seconds='0'+seconds;
-    }
-    if (minutes<10){
-      minutes='0'+minutes;
-    }
-    if (hours<10){
-      hours='0'+hours;
-    }
-    
-    time.textContent = ` ${left_days} days ${left_hours} hours ${left_minutes} minutes ${left_seconds} seconds`;
-
-    sana.textContent = `Bugun ${kunlari[hafta]}, ${kun}-${months[mon]}, ${year}-yil`
-    vaqt.textContent =`${hours}:${minutes}:${seconds}`
-
-  },1000);
-
-})();
+    document.body.innerHTML = userListHTML;
+  })
+  .catch(error => {
+    console.error('Xatolik yuz berdi:', error);
+  });
 
 
-// 123 days 4 hours 56 minutes 23 seconds left
- 
+async function getUsers() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+  
+    let userListHTML2 = '<ul>';
+    users.forEach(user => {
+      userListHTML2 += `
+        <li>
+          <strong>Name:</strong> ${user.name}<br>
+          <strong>Email:</strong> ${user.email}<br>
+          <strong>Phone:</strong> ${user.phone}<br>
+          <strong>Street:</strong> ${user.address.street}<br>
+        </li>
+      `;
+    });
+    userListHTML2 += '</ul>';
+  
+    document.body.innerHTML = userListHTML2;
+  } catch (error) {
+    console.error('Xatolik yuz berdi:', error);
+  }
+}
+
+getUsers();
